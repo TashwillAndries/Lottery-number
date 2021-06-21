55335bd210788a5645db2d5629f9a5ce7e817eb7
Tashwill <tashwilla27@gmail.com>
2021-06-21 16:21:01 +0200

    updates

--- test2.py
+++ test2.py
@@ -3,4 +3,18 @@ import unittest
 def Winnings():
     earnings1 = 20
     earnings2 = 20
-    earnings3 = 20
+    earnings3 = 10
+    winnings = earnings1 + earnings2 + earnings3
+    print(winnings)
+    return winnings
+
+
+# checking if the function runs
+class AddingWinnings(unittest.TestCase):
+    def adding(self):
+        results = Winnings()
+        self.assertEqual(results, 60)
+
+
+if __name__ == '__main__':
+    unittest.main()
